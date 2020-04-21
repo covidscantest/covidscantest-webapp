b02e41d1ce4c170b74ff9db33ce755e8e0703c74
Anatoly Shikolay <shikolay@gmail.com>
2020-04-20 20:38:20 -0500

    add support for pytorch model

--- covidwebapp/scandetector/urls.py
+++ covidwebapp/scandetector/urls.py
@@ -6,6 +6,7 @@ from .views import ScanProcessView
 app_name = 'scandetector'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('result/', ScanProcessView.as_view(), name='result'),
     path('how-it-works', TemplateView.as_view(template_name='how-it-works.html'), name='how'),
     path('team', TemplateView.as_view(template_name='team.html'), name='team'),
 ]
--- covidwebapp/scandetector/views.py
+++ covidwebapp/scandetector/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render
 from django.views import View
 from .models import ScanUpload
-import tensorflow as tf
+import pickle
+import torch
+import torchvision.models as torchmodels
+import torchvision.transforms as transforms
 import numpy
 from PIL import Image
 
+model_obj = pickle.load(open('./scandetector/weights/pytorch_inceptionv3_final.pckl', 'rb'))
+torch_model = torchmodels.inception.inception_v3(num_classes=3)
+torch_model.load_state_dict(model_obj['model_state_dict'])
+torch_model.eval()
 
-model = tf.keras.models.load_model('./scandetector/weights/DenseNet121_model.hdf5')
-input_size = (224, 224)
 
 class ScanProcessView(View):
     template_name = 'result.html'
 
     def post(self, request):
         image_upload = request.FILES['image']
-        pil_image = Image.open(image_upload.file)
-        resized_image = pil_image.resize(input_size)
-
-        img_as_arr = numpy.asarray(resized_image)
-        img_input = numpy.expand_dims(img_as_arr, axis=0)
-        predicted_probs = model.predict_proba(img_input)
+        pil_image = Image.open(image_upload.file).convert('RGB')
+        transform = transforms.Compose(
+            [
+                transforms.Resize(299),
+                transforms.CenterCrop(299),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+        norm_img = transform(pil_image)
+        input_tensor = norm_img.view(1, 3, 299, 299)
+        output_tensor = torch_model(input_tensor)
+        predicted_probs_tensor = torch.nn.functional.softmax(output_tensor[0], dim=0)
 
         new_upload = ScanUpload(scan_file=image_upload)
         new_upload.save()
 
-        classes_list = predicted_probs.tolist()
+        classes_list = predicted_probs_tensor.tolist()
 
         return render(request, self.template_name, {'classes': classes_list})
