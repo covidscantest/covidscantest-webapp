66859da2dcc2c74890742b8ea820da970eeb01f3
unknown <mrloganellis@gmail.com>
2020-05-03 12:49:31 -0400

    added a privacy policy and checkboxes on submission

--- covidwebapp/scandetector/urls.py
+++ covidwebapp/scandetector/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('result/', ScanProcessView.as_view(), name='result'),
     path('how-it-works', TemplateView.as_view(template_name='how-it-works.html'), name='how'),
     path('team', TemplateView.as_view(template_name='team.html'), name='team'),
+    path('privacypolicy', TemplateView.as_view(template_name='privacy-policy.html'), name='privacy-policy'),
 ]
